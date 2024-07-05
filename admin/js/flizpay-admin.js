d09b62d036e7f6ded353f4fb9bb4f2f977a6061d
Thrashattack <henrique202@gmail.com>
2024-07-05 23:31:13 +0200

    feat: add fields and necessary JS and CSS

--- admin/js/flizpay-admin.js
+++ admin/js/flizpay-admin.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
 
 	/**
@@ -28,30 +28,47 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
-	jQuery(document).ready(function($) {
-    $('#woocommerce_flizpay_test_connection').after('<button id="test_connection_button" type="button">Test Connection</button>');
+	jQuery(document).ready(function ($) {
+		const testButton = document.createElement('div');
+		const resultField = document.createElement('div');
+		const apiKeyInput = document.querySelector('#woocommerce_flizpay_flizpay_api_key');
+		const webhookURLInput = document.querySelector('#woocommerce_flizpay_flizpay_webhook_url');
+		testButton.setAttribute('id', 'woocommerce_flizpay_test_connection');
+		resultField.setAttribute('id', 'woocommerce_flizpay_connection_result')
+		apiKeyInput.parentNode.appendChild(testButton);
+		apiKeyInput.parentNode.appendChild(resultField);
+		testButton.innerHTML = '<button id="test_connection_button" type="button">Test Connection</button>'
+		webhookURLInput.setAttribute('disabled', true);
 
-    $('#test_connection_button').on('click', function() {
-        var apiKey = $('#woocommerce_flizpay_api_key').val();
+		$('#test_connection_button').on('click', function () {
+			var nonce = flizpayParams.nonce;
 
-        $.ajax({
-            url: 'api.flizpay.de/test-connection',
-            method: 'POST',
-            data: {
-                action: 'test_gateway_connection',
-                api_key: apiKey,
-            },
-            success: function(response) {
-                if (response.success) {
-                    alert('Connection successful: ' + response.data);
-                } else {
-                    alert('Connection failed: ' + response.data);
-                }
-            },
-            error: function() {
-                alert('An error occurred while testing the connection.');
-            }
-        });
-    });
-});
+			$.ajax({
+				url: ajaxurl,
+				method: 'POST',
+				data: {
+					action: 'test_gateway_connection',
+					api_key: apiKeyInput.value,
+					nonce: nonce,
+				},
+				success: function (response) {
+					if (response.success) {
+						resultField.classList.add('connection-success')
+						testButton.classList.add('hidden')
+						apiKeyInput.setAttribute('disabled', 'true')
+						resultField.innerHTML = 'Connected and ready to use!'
+						webhookURLInput.value = response.data.webhookUrl
+					} else {
+						resultField.classList.add('connection-failed')
+						resultField.innerHTML = response.data;
+					}
+				},
+				error: function (e) {
+					resultField.classList.add('connection-failed')
+					resultField.innerHTML = 'An error occurred while testing the connection. ' + JSON.stringify(e);
+				}
+			});
+		});
+	});
+
 })(jQuery);
