d7c493c810dd07493d932e01bc76f97cb547ec04
Thrashattack <henrique202@gmail.com>
2024-07-08 17:02:20 +0200

    fix: link

--- includes/class-flizpay-i18n.php
+++ includes/class-flizpay-i18n.php
@@ -24,7 +24,8 @@
  * @subpackage Flizpay/includes
  * @author     Flizpay <roberto.ammirata@flizpay.de>
  */
-class Flizpay_i18n {
+class Flizpay_i18n
+{
 
 
 	/**
@@ -32,12 +33,13 @@ class Flizpay_i18n {
 	 *
 	 * @since    1.0.0
 	 */
-	public function load_plugin_textdomain() {
+	public function load_plugin_textdomain()
+	{
 
 		load_plugin_textdomain(
 			'flizpay',
 			false,
-			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+			dirname(dirname(plugin_basename(__FILE__))) . '/languages/'
 		);
 
 	}
