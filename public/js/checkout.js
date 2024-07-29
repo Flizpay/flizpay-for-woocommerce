ba34cabf5677d8fea634d5e26b10db0a520a07ee
Thrashattack <henrique202@gmail.com>
2024-07-29 15:15:08 +0200

    fix: lint

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,21 +1,21 @@
 // const settings= window.wc.wcSettings.getSetting( 'woocommerce_flizpay_settings', {} );
-const settings= window.wc.wcSettings.allSettings.paymentMethodData.flizpay;
+const settings = window.wc.wcSettings.allSettings.paymentMethodData.flizpay;
 
-const label = window.wp.htmlEntities.decodeEntities( settings.title );
+const label = window.wp.htmlEntities.decodeEntities(settings.title);
 const Content = () => {
-    return window.wp.htmlEntities.decodeEntities ( settings.description ||'' );
-}
-
-const Flizpay_Gateway = {
-    name: 'flizpay',
-    label: label,
-    content: Object(window.wp.element.createElement)(Content, null),
-    edit: Object(window.wp.element.createElement)(Content, null),
-    canMakePayment: () => true,
-    ariaLabel: label,
-    supports: {
-        features: ['products'],//settings.supports,
-    },
+  return window.wp.htmlEntities.decodeEntities(settings.description || "");
 };
 
-window.wc.wcBlocksRegistry.registerPaymentMethod( Flizpay_Gateway )
+const Flizpay_Gateway = {
+  name: "flizpay",
+  label: label,
+  content: Object(window.wp.element.createElement)(Content, null),
+  edit: Object(window.wp.element.createElement)(Content, null),
+  canMakePayment: () => true,
+  ariaLabel: label,
+  supports: {
+    features: ["products"], //settings.supports,
+  },
+};
+
+window.wc.wcBlocksRegistry.registerPaymentMethod(Flizpay_Gateway);
