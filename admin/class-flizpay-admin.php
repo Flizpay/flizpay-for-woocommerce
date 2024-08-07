f9f4f67acfce4c36cff4031ef9dace1240feb892
Thrashattack <henrique202@gmail.com>
2024-08-07 22:58:45 +0200

    feat: use double language on admin

--- admin/class-flizpay-admin.php
+++ admin/class-flizpay-admin.php
@@ -18,7 +18,7 @@
  *
  * @package    Flizpay
  * @subpackage Flizpay/admin
- * @author     Flizpay <roberto.ammirata@flizpay.de>
+ * @author     Flizpay <carlos.cunha@flizpay.de>
  */
 class Flizpay_Admin
 {
@@ -126,36 +126,23 @@ class Flizpay_Admin
 	{
 		return array(
 			'description_banner' => array(
-				'title' => __('', 'flizpay'), // Empty title, used for HTML output
+				'title' => '', // Empty title, used for HTML output
 				'type' => 'title', // Using 'title' as a workaround to insert HTML
 				'description' => $this->add_description_banner(), // Including HTML content via a method
 			),
 			'flizpay_enabled' => array(
-				'title' => 'Enable/Disable',
-				'label' => 'Enable Flizpay Gateway',
+				'title' => 'Ermöglicht<br><p style="font-style: italic;">Enabled</p>',
+				'label' => 'FLIZpay aktiviert<br><p style="font-style: italic;">FLIZpay enabled</p>',
 				'type' => 'checkbox',
 				'description' => '',
 				'default' => 'no',
 			),
-			'title' => array(
-				'title' => 'Title',
-				'type' => 'text',
-				'description' => 'This controls the title which the user sees during checkout.',
-				'default' => 'FLIZpay',
-				'desc_tip' => true,
-			),
-			'description' => array(
-				'title' => 'Description',
-				'type' => 'textarea',
-				'description' => 'This controls the description which the user sees during checkout.',
-				'default' => 'Pay securely with your bank account via the FLIZ app.',
-				'desc_tip' => true,
-			),
 			'flizpay_api_key' => array(
 				'title' => 'API KEY',
 				'label' => 'Enter API KEY',
-				'type' => 'text',
-				'description' => 'Enter you API KEY. This information is very sensitive, it should be treated as a password.',
+				'type' => 'password',
+				'description' => 'Gib deinen API KEY ein.  Der API KEY ist ein sensibler Datensatz und sollte wie ein Passwort behandelt werden.<br>
+				<p style="font-style: italic;">Enter you API KEY. This information is very sensitive, it should be treated as a password.</p>',
 				'desc_tip' => false,
 			),
 			'flizpay_webhook_url' => array(
@@ -166,9 +153,10 @@ class Flizpay_Admin
 				'desc_tip' => false,
 			),
 			'flizpay_webhook_alive' => array(
-				'title' => '',
+				'title' => 'Verbindung hergestellt.<br><p style="font-style: italic;">Connection Stablished</p>',
 				'type' => 'checkbox',
-				'description' => 'This indicates when our servers manage to communicate with your site via the webhook URL. Reload the page a few seconds after testing the connection.',
+				'label' => '<div id="connection-stablished-description">Dies zeigt an, wann unsere Server über die Webhook-URL mit Ihrer Site kommunizieren können. Laden Sie die Seite einige Sekunden nach dem Testen der Verbindung neu.<br>
+				<p style="font-style: italic;">This indicates when our servers manage to communicate with your site via the webhook URL. Reload the page a few seconds after testing the connection.</p></div>',
 				'default' => 'no',
 				'desc_tip' => false,
 			),
@@ -178,28 +166,51 @@ class Flizpay_Admin
 	private function add_description_banner()
 	{
 		return <<<HTML
-			<h2>Flizpay</h2>
-			<div style="background-color: #f7f7f7; margin-bottom: 20px; padding: 20px; border-left: 4px solid #007cba;">
-				<div style="width: 100%; display: flex; justify-content: center; flex-wrap: wrap; text-align: center;">
-					<h2 style="width: 100%; text-align: center;">Welcome to the FLIZpay WooCommerce plugin!</h2>
+			<div class='flizpay-description-banner'>
+				<div class="flizpay-german-banner">
+					<div style="flizpay-banner-header">
+						<p class="flizpay-header-text">Willkommen bei FLIZpay in WooCommerce!</p>
+					</div>
+					<p>Anleitung:</p>
+					<ol>
+						<li>Wenn du noch kein FLIZ-Firmenkonto hast, klicke bitte <a href='app.flizpay.de' target='_blank'>hier</a> und registriere dich jetzt.</li>
+						<li>Nachdem du dich registriert hast, generiere bitte einen neuen API Key in deinem FLIZ-Firmenkonto und füge ihn in das unten stehende Feld “API KEY” ein. Du findest den API-Schlüssel im Abschnitt “Installation” deines FLIZ-Firmenkontos.</li>
+						<li>Klicke anschließend auf den Button “Änderungen speichern”.</li>
+						<li>Warte etwa 5 Sekunden, bis die Seite automatisch neu lädt.</li>
+						<li>Nach dem Neuladen der Seite erscheint unten eine grüne Box, die die erfolgreiche Konfiguration des FLIZpay-Plugins anzeigt. Du bist jetzt bereit, gebührenfreie Zahlungen zu empfangen.</li>
+						<li>Stelle sicher, dass du mit FLIZ so viele Gebühren wie möglich sparst. Vergiss also nicht, FLIZpay in der für Kunden zur Auswahl stehenden Liste an Zahlungsmethoden an die erste Stelle zu setzen. Das kannst du durch auf deiner WordPress-Admin Seite. Klicke im linken Menü auf “WooCommerce”, dann im Untermenü auf “Einstellungen”. Klicke anschließend im Menü oben auf den Tab “Zahlungen”. Auf dieser Seite kannst du per Drag-and-Drop oder mit den Pfeilen die Position von FLIZpay in der Liste der verfügbaren Zahlungsmethoden anpassen. Klicke zum Schluss unten links auf “Änderungen speichern”.</li>
+						<li>Darüber hinaus empfehlen wir dir, Cashback für deine Kunden zu aktivieren. Ändere das Zahlungsverhalten deiner Kunden, damit sie FLIZ nutzen - denn FLIZ ist gebührenfrei. Um das zu erreichen, kannst du einen prozentualen Cashback festlegen, der als Rabatt von jedem Einkauf abgezogen wird, den deine Kunden mit FLIZ tätigen. Je mehr Cashback du gibst, desto höher wird der Anteil von FLIZ an deiner Kasse, was bedeutet, dass deine Kundenbasis zu einer gebührenfreien Zahlungsmethode gewechselt ist. Du kannst Cashback in deinem FLIZ-Firmenkonto aktivieren, <a href='app.flizpay.de' target='_blank'>klicke hier.</a></li>
+					</ol>
+					
+					<p>
+						Wenn du mehr darüber erfahren möchtest, wie du dieses Plugin für deine Bedürfnisse konfigurieren kannst, <a 
+							href="https://www.docs.flizpay.de/docs/intro"
+							target="_blank"
+							>schau dir unsere Dokumentation an.</a>
+					</p>
 				</div>
-				<p>Instructions:</p>
-				<ol>
-					<li>If you haven’t signed up for a FLIZ company account, please <a href="https://www.flizpay.de/signup" target="_blank">click here</a> and sign up.</li>
-					<li>Once you signed up, please generate a new <strong>API Key</strong> in your FLIZ company account and paste it in the “API KEY” field below.</li>
-					<li>Click the button <strong>Connect to FLIZpay</strong></li>
-					<li>Wait about 5 seconds and the page will reload automatically.</li>
-					<li>After the page reload, a green box is shown at the bottom, indicating the successful configuration of the FLIZpay plugin. You are now ready to receive fee-free payments.</li>
-					<li>Make sure you save as many fees as possible with our fee-free payments. So, don’t forget to put FLIZpay first in the list of payment methods presented to customers. To do so, click on “Payments” at the top of this page. There, you can drag and drop or use the arrows to fix the position of a payment method in the list of available methods.</li>
-					<li>Clicking on “Save changes” on the bottom left is optional.</li>
-				</ol>
-				
-				<p>
-					If you'd like to know more about how to configure this plugin for your needs, <a 
-						href="https://www.docs.flizpay.de/docs/intro"
-						target="_blank"
-						>check out our documentation.</a>
-				</p>
+				<div class="flizpay-english-banner">
+					<div style="flizpay-banner-header">
+						<p class="flizpay-header-text">Welcome to FLIZpay in WooCommerce!</p>
+					</div>
+					<p>Instructions:</p>
+					<ol>
+						<li>If you haven’t signed up for a FLIZ company account, please <a href='app.flizpay.de' target='_blank'>click here</a> and sign up.</li>
+						<li>Once you have signed up, please generate a new API Key in your FLIZ company account and paste it in the “API KEY” field below. You will find the API KEY in the “Installation” section of your company account.</li>
+						<li>Afterwards, click the button “Save changes”.</li>
+						<li>Wait about 5 seconds and the page will reload automatically.</li>
+						<li>After the page reloaded, a green box will appear at the bottom, indicating the successful configuration of the FLIZpay plugin. You are now ready to receive fee-free payments.</li>
+						<li>Make sure you save as many fees as possible with fee-free FLIZ. Don’t forget to put FLIZpay first in the list of payment methods presented to customers. To do this go to the left menu and click on “WooCommerce”, then click on the sub-menu “Settings”. Now click on the tab “Payments” in the menu at the top of the page. Here, you can drag and drop or use the arrows to adjust the position of FLIZpay in the list of available payment methods. When you are finished, go to the bottom left of that page and click “Save changes”.</li>
+						<li>Moreover, we advise you to activate cashback for your customers. With cashback, you change your customers’ payment behavior to ensure they use fee-free payment method FLIZ. This way, customers support your bottom line and optimize their own finances at the same time. You can activate cashback in your FLIZ company account, <a href='app.flizpay.de' target='_blank'>click here.</a></li>
+					</ol>
+					
+					<p>
+						If you'd like to know more about how to configure this plugin for your needs, <a 
+							href="https://www.docs.flizpay.de/docs/intro"
+							target="_blank"
+							>check out our documentation.</a>
+					</p>
+				</div>
 			</div>
 		HTML;
 	}
