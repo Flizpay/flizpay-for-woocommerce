860aa13b5206f140517fba0d66398cc31710088e
Thrashattack <henrique202@gmail.com>
2024-08-18 15:11:27 +0200

    fix: do not direct SQL, don't change schema

--- uninstall.php
+++ uninstall.php
@@ -1,77 +1,48 @@
 <?php
 
-/**
- * Fired when the plugin is uninstalled.
- *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future versions of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       https://www.flizpay.de
- * @since      1.0.0
- *
- * @package    Flizpay
- */
-
-// If uninstall not called from WordPress, then exit.
 if (!defined('WP_UNINSTALL_PLUGIN')) {
 	exit;
 }
 
-// ** Clean up options **
-delete_option('flizpay_api_key');
-delete_option('flizpay_webhook_key');
-delete_option('flizpay_webhook_url');
-delete_option('flizpay_enabled');
-delete_option('flizpay_webhook_alive');
-delete_option('woocommerce_flizpay_settings');
+// Clean up options
+$options = array(
+	'flizpay_api_key',
+	'flizpay_webhook_key',
+	'flizpay_webhook_url',
+	'flizpay_enabled',
+	'flizpay_webhook_alive',
+	'woocommerce_flizpay_settings',
+);
 
-// ** For multisite: Delete options across all sites **
+foreach ($options as $option) {
+	delete_option($option);
+}
+
+// For multisite: Delete options across all sites
 if (is_multisite()) {
-	global $wpdb;
-	$blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
+	$blog_ids = get_sites(array('fields' => 'ids'));
 	foreach ($blog_ids as $blog_id) {
 		switch_to_blog($blog_id);
-		delete_option('flizpay_api_key');
-		delete_option('flizpay_webhook_key');
-		delete_option('flizpay_webhook_url');
-		delete_option('flizpay_enabled');
-		delete_option('flizpay_webhook_alive');
-		delete_option('woocommerce_flizpay_settings');
+		foreach ($options as $option) {
+			delete_option($option);
+		}
 		restore_current_blog();
 	}
 }
 
-// ** Clean up custom tables **
-global $wpdb;
-$table_name = $wpdb->prefix . 'flizpay_custom_table';
-$wpdb->query("DROP TABLE IF EXISTS $table_name");
-
-// ** Clean up custom post types **
+// Clean up custom post types
 $custom_post_types = array('flizpay_custom_post_type');
 foreach ($custom_post_types as $post_type) {
-	$posts = get_posts(
-		array(
-			'post_title' => wp_strip_all_tags('Flizpay Payment Fail'),
-		)
-	);
+	$posts = get_posts(array(
+		'post_type' => $post_type,
+		'post_status' => 'any',
+		'numberposts' => -1
+	));
 
 	foreach ($posts as $post) {
 		wp_delete_post($post->ID, true);
 	}
 }
 
-// ** Clean up transients **
+// Clean up transients
 delete_transient('flizpay_transient');
