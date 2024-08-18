b89bcfa50934d3e8b1d8a82d49db6ac6e04299cc
Thrashattack <henrique202@gmail.com>
2024-08-18 15:10:37 +0200

    fix: change text domain to flizpay-for-woocommerce

--- flizpay.php
+++ flizpay.php
@@ -21,7 +21,7 @@
  * Author URI:        https://www.flizpay.de/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       flizpay-gateway
+ * Text Domain:       flizpay-for-woocommerce
  * Domain Path:       /languages
  */
 
