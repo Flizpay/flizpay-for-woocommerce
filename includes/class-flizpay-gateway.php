582e6559625e250e955933b9b1d11975b13c165c
Carlos Cunha <henrique2092@gmail.com>
2024-10-22 12:48:00 +0200

    release: bump version to 1.2.2 (#54)
    Co-authored-by: Thrashattack <henrique202@gmail.com>

--- includes/class-flizpay-gateway.php
+++ includes/class-flizpay-gateway.php
@@ -8,7 +8,7 @@ function flizpay_init_gateway_class()
 
     class WC_Flizpay_Gateway extends WC_Payment_Gateway
     {
-        static $VERSION = "1.2.1";
+        static $VERSION = "1.2.2";
 
         public $icon;
         public $title;
