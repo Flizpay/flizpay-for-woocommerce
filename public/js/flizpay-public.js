887cca0d880caf516eede5ea05da27f05f9419dd
Carlos Cunha <henrique2092@gmail.com>
2024-10-23 11:26:34 +0200

    feat: implement local storage to resume polling (#53)
    * feat: add cancel button to loading wheel.
    Improve readability
    
    * feat: add resume polling with localstorage
    
    ---------
    
    Co-authored-by: Thrashattack <henrique202@gmail.com>

--- public/js/flizpay-public.js
+++ public/js/flizpay-public.js
@@ -10,7 +10,7 @@
 
 jQuery(function ($) {
   $(document).ready(function () {
-    let stopPolling = false;
+    let stopPolling = localStorage.getItem("flizpay_stop_polling") || false;
     try {
       const cancelButtonLabel = navigator.language.includes("en")
         ? "Cancel"
@@ -35,9 +35,13 @@ jQuery(function ($) {
       setTimeout(() => {
         const isBLocks = document.querySelector("form.wc-block-checkout__form");
         const isClassic = document.querySelector("form.woocommerce-checkout");
+        const existentOrderId = localStorage.getItem("flizpay_order_id");
 
         if (isBLocks) bind_blocks_hook();
         if (isClassic) bind_ajax_hook(isClassic);
+
+        if (existentOrderId)
+          mobile_redirect_when_order_finished(existentOrderId);
       }, 1700);
 
       const bind_blocks_hook = () => {
@@ -55,6 +59,9 @@ jQuery(function ($) {
             mobile_redirect_when_order_finished(
               wp.data.select("wc/store/checkout").getOrderId()
             );
+            updateLocalStorage(
+              wp.data.select("wc/store/checkout").getOrderId()
+            );
             unsubscribe();
           }
         });
@@ -70,17 +77,22 @@ jQuery(function ($) {
           jQuery(document).ajaxComplete(function (_, xhr, settings) {
             if (settings.url.indexOf("wc-ajax=checkout") !== -1) {
               const response = xhr.responseJSON;
-
               if (response && response.result === "success") {
                 if (response.order_id) {
                   stopPolling = false;
                   fliz_block_ui();
                   mobile_redirect_when_order_finished(response.order_id);
+                  updateLocalStorage(response.order_id);
                 }
               }
             }
           });
         });
+      };
+
+      const updateLocalStorage = (order_id) => {
+        localStorage.removeItem("flizpay_order_id");
+        localStorage.setItem("flizpay_order_id", order_id);
       };
 
       const fliz_block_ui = () => {
@@ -112,6 +124,8 @@ jQuery(function ($) {
       const mobile_redirect_when_order_finished = (order_id) => {
         if (stopPolling) {
           jQuery.unblockUI();
+          localStorage.removeItem("flizpay_stop_polling");
+          localStorage.removeItem("flizpay_order_id");
           return;
         }
 
@@ -141,6 +155,7 @@ jQuery(function ($) {
               ) {
                 mobile_redirect_when_order_finished(order_id);
               } else {
+                localStorage.setItem("flizpay_stop_polling", true);
                 window.location.href = order.url;
               }
             }, 2000);
